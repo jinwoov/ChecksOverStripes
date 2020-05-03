b9b25b1e71e467ff897debb6258ef07acf218282
Jin Kim <jinwoov@gmail.com>
2020-05-03 16:33:55 -0700

    created controllers

---
+++ API/API/Controllers/CartItemsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartItemsController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public CartItemsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CartItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartItems>>> GetCartItems()
+        {
+            return await _context.CartItems.ToListAsync();
+        }
+
+        // GET: api/CartItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CartItems>> GetCartItems(int id)
+        {
+            var cartItems = await _context.CartItems.FindAsync(id);
+
+            if (cartItems == null)
+            {
+                return NotFound();
+            }
+
+            return cartItems;
+        }
+
+        // PUT: api/CartItems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCartItems(int id, CartItems cartItems)
+        {
+            if (id != cartItems.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cartItems).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartItemsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CartItems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<CartItems>> PostCartItems(CartItems cartItems)
+        {
+            _context.CartItems.Add(cartItems);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCartItems", new { id = cartItems.ID }, cartItems);
+        }
+
+        // DELETE: api/CartItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CartItems>> DeleteCartItems(int id)
+        {
+            var cartItems = await _context.CartItems.FindAsync(id);
+            if (cartItems == null)
+            {
+                return NotFound();
+            }
+
+            _context.CartItems.Remove(cartItems);
+            await _context.SaveChangesAsync();
+
+            return cartItems;
+        }
+
+        private bool CartItemsExists(int id)
+        {
+            return _context.CartItems.Any(e => e.ID == id);
+        }
+    }
+}
