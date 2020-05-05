152e120c7b200b03c66e1545eda91ede34d3c00c
Jin Kim <jinwoov@gmail.com>
2020-05-04 23:06:34 -0700

    Everything in place

---
+++ MVC/MVC/Data/ApplicationUserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Data
+{
+    public class ApplicationUserDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationUserDbContext(DbContextOptions<ApplicationUserDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- MVC/MVC/Startup.cs
+++ MVC/MVC/Startup.cs
@@ -1,21 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVC.Data;
 
 namespace MVC
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+           .AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddRazorPages();
+
+            services.AddDbContext<ApplicationUserDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
